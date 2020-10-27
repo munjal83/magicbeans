eb207f6098bfadd787e8ca2640ad733ddb312ba9
munjal83 <munjalbpatel@hotmail.com>
2020-10-27 15:23:41 -0400

    move shop, directory state to redux

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -2,57 +2,23 @@ import React from 'react';
 import MenuItem from '../menuitem/MenuItem';
 import './Directory.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-    this.state = {
-      sections: [
-        {
-          title: 'beverages',
-          imageUrl: 'https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-          id: 1,
-          linkUrl: 'beverages',
-        },
-        {
-          title: 'merchandise',
-          imageUrl: 'https://images.pexels.com/photos/373888/pexels-photo-373888.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-          id: 2,
-          linkUrl: 'shop/merchandise',
-        },
-        {
-          title: 'food',
-          imageUrl: 'https://images.pexels.com/photos/6194/bread-food-baking-homemade.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-          id: 3,
-          linkUrl: 'shop/food',
-        },
-        {
-          title: 'coffee',
-          imageUrl: 'https://images.pexels.com/photos/1695052/pexels-photo-1695052.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/coffee',
-        },
-        {
-          title: 'tea',
-          imageUrl: 'https://images.pexels.com/photos/227908/pexels-photo-227908.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/tea',
-        },
-      ],
-    };
-  }
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps}) => (
-          <MenuItem key={id} {...otherSectionProps}/>
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
---
+++ src/pages/collection/Collection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/CollectionItem'
+
+import './Collection.scss'
+
+const Collection = ({ match }) => {
+    console.log(match)
+
+    return (
+        <div className='collection-page'>
+            <h2>CATEGORY PAGE</h2>
+        </div>
+    )
+}
+
+export default Collection
