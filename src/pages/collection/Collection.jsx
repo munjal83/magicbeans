9155fb46189853190cad1f14500ec2d41efd2448
munjal83 <munjalbpatel@hotmail.com>
2020-11-02 12:24:06 -0500

    Add collection page and move directory and shop data to redux and define their own selectors and reducers

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import CollectionItem from '../../components/collection-item/CollectionItem'
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
-import './Collection.scss'
+import CollectionItem from '../../components/collection-item/CollectionItem';
 
-const Collection = ({ match }) => {
-    console.log(match)
+import './Collection.scss';
 
-    return (
-        <div className='collection-page'>
-            <h2>CATEGORY PAGE</h2>
-        </div>
-    )
-}
+const Collection = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Collection
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(Collection);
